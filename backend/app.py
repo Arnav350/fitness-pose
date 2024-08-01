3f29ab4027fb4e7421dbce21c8e8b2a1ad25e703
Arnav Patel <patelarnavm+github@gmail.com>
2024-07-31 23:28:33 -0400

    added triceps quads and hamstrings

--- backend/app.py
+++ backend/app.py
@@ -29,9 +29,10 @@ right_counter = 0
 left_stage = None
 right_stage = None
 running = False
+exercise_mode = 'biceps'  # Default mode
 
 def pose_detection():
-    global left_counter, right_counter, left_stage, right_stage, running
+    global left_counter, right_counter, left_stage, right_stage, running, exercise_mode
     running = True
     cap = cv2.VideoCapture(0)
 
@@ -53,71 +54,185 @@ def pose_detection():
             image.flags.writeable = True
             image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-            try:
-                landmarks = results.pose_landmarks.landmark
+            if results.pose_landmarks is not None:
+                try:
+                    landmarks = results.pose_landmarks.landmark
 
-                # Left side
-                left_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
-                                 landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
-                left_elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
-                              landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
-                left_wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
-                              landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+                    if exercise_mode in ['biceps', 'triceps']:
+                        # Left side
+                        left_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
+                                         landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+                        left_elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
+                                      landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+                        left_wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
+                                      landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
 
-                left_angle = calculate_angle(left_shoulder, left_elbow, left_wrist)
+                        left_angle = calculate_angle(left_shoulder, left_elbow, left_wrist)
 
-                if left_angle > 160:
-                    left_stage = "isometric bottom"
-                elif left_angle < 40:
-                    left_stage = "isometric top"
-                elif 40 <= left_angle <= 160:
-                    if left_stage == "isometric bottom":
-                        left_stage = "concentric"
-                    elif left_stage == "isometric top":
-                        left_stage = "eccentric"
-                        left_counter += 1
-                        print("Left reps:", left_counter)
+                        # Right side
+                        right_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
+                                          landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+                        right_elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,
+                                       landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+                        right_wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,
+                                       landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
 
-                # Draw angle on the image
-                cv2.putText(image, str(int(left_angle)),
-                            tuple(np.multiply(left_elbow, [640, 480]).astype(int)),
-                            cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA
-                            )
+                        right_angle = calculate_angle(right_shoulder, right_elbow, right_wrist)
 
-                # Right side
-                right_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
-                                  landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
-                right_elbow = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,
-                               landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
-                right_wrist = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,
-                               landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+                        if exercise_mode == 'biceps':
+                            # Logic for biceps
+                            if left_angle > 160:
+                                left_stage = "isometric bottom"
+                            elif left_angle < 40:
+                                if left_stage == "concentric":
+                                    left_counter += 1
+                                    print("Left reps:", left_counter)
+                                left_stage = "isometric top"
+                            elif 40 <= left_angle <= 160:
+                                if left_stage == "isometric bottom":
+                                    left_stage = "concentric"
+                                elif left_stage == "isometric top":
+                                    left_stage = "eccentric"
 
-                right_angle = calculate_angle(right_shoulder, right_elbow, right_wrist)
+                            if right_angle > 160:
+                                right_stage = "isometric bottom"
+                            elif right_angle < 40:
+                                if right_stage == "concentric":
+                                    right_counter += 1
+                                    print("Right reps:", right_counter)
+                                right_stage = "isometric top"
+                            elif 40 <= right_angle <= 160:
+                                if right_stage == "isometric bottom":
+                                    right_stage = "concentric"
+                                elif right_stage == "isometric top":
+                                    right_stage = "eccentric"
+                        elif exercise_mode == 'triceps':
+                            # Logic for triceps
+                            if left_angle < 40:
+                                left_stage = "isometric bottom"
+                            elif left_angle > 160:
+                                if left_stage == "concentric":
+                                    left_counter += 1
+                                    print("Left reps:", left_counter)
+                                left_stage = "isometric top"
+                            elif 40 <= left_angle <= 160:
+                                if left_stage == "isometric top":
+                                    left_stage = "eccentric"
+                                elif left_stage == "isometric bottom":
+                                    left_stage = "concentric"
 
-                if right_angle > 160:
-                    right_stage = "isometric bottom"
-                elif right_angle < 40:
-                    right_stage = "isometric top"
-                elif 40 <= right_angle <= 160:
-                    if right_stage == "isometric bottom":
-                        right_stage = "concentric"
-                    elif right_stage == "isometric top":
-                        right_stage = "eccentric"
-                        right_counter += 1
-                        print("Right reps:", right_counter)
+                            if right_angle < 40:
+                                right_stage = "isometric bottom"
+                            elif right_angle > 160:
+                                if right_stage == "concentric":
+                                    right_counter += 1
+                                    print("Right reps:", right_counter)
+                                right_stage = "isometric top"
+                            elif 40 <= right_angle <= 160:
+                                if right_stage == "isometric top":
+                                    right_stage = "eccentric"
+                                elif right_stage == "isometric bottom":
+                                    right_stage = "concentric"
+                    elif exercise_mode in ['quads', 'hamstrings']:
+                        # Left side
+                        left_hip = [landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].x,
+                                    landmarks[mp_pose.PoseLandmark.LEFT_HIP.value].y]
+                        left_knee = [landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].x,
+                                     landmarks[mp_pose.PoseLandmark.LEFT_KNEE.value].y]
+                        left_ankle = [landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].x,
+                                      landmarks[mp_pose.PoseLandmark.LEFT_ANKLE.value].y]
 
-                # Draw angle on the image
-                cv2.putText(image, str(int(right_angle)),
-                            tuple(np.multiply(right_elbow, [640, 480]).astype(int)),
-                            cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA
-                            )
+                        left_angle = calculate_angle(left_hip, left_knee, left_ankle)
 
-                # Draw landmarks and connections on the image
-                mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+                        # Right side
+                        right_hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,
+                                     landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+                        right_knee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,
+                                      landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
+                        right_ankle = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x,
+                                       landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
 
-            except Exception as e:
-                print(e)
-                continue
+                        right_angle = calculate_angle(right_hip, right_knee, right_ankle)
+
+                        if exercise_mode == 'quads':
+                            # Logic for quads
+                            if left_angle < 60:
+                                left_stage = "isometric bottom"
+                            elif left_angle > 160:
+                                if left_stage == "concentric":
+                                    left_counter += 1
+                                    print("Left reps:", left_counter)
+                                left_stage = "isometric top"
+                            elif 60 <= left_angle <= 160:
+                                if left_stage == "isometric top":
+                                    left_stage = "eccentric"
+                                elif left_stage == "isometric bottom":
+                                    left_stage = "concentric"
+
+                            if right_angle < 60:
+                                right_stage = "isometric bottom"
+                            elif right_angle > 160:
+                                if right_stage == "concentric":
+                                    right_counter += 1
+                                    print("Right reps:", right_counter)
+                                right_stage = "isometric top"
+                            elif 60 <= right_angle <= 160:
+                                if right_stage == "isometric top":
+                                    right_stage = "eccentric"
+                                elif right_stage == "isometric bottom":
+                                    right_stage = "concentric"
+                        elif exercise_mode == 'hamstrings':
+                            # Logic for hamstrings
+                            if left_angle > 160:
+                                left_stage = "isometric bottom"
+                            elif left_angle < 60:
+                                if left_stage == "concentric":
+                                    left_counter += 1
+                                    print("Left reps:", left_counter)
+                                left_stage = "isometric top"
+                            elif 60 <= left_angle <= 160:
+                                if left_stage == "isometric bottom":
+                                    left_stage = "concentric"
+                                elif left_stage == "isometric top":
+                                    left_stage = "eccentric"
+
+                            if right_angle > 160:
+                                right_stage = "isometric bottom"
+                            elif right_angle < 60:
+                                if right_stage == "concentric":
+                                    right_counter += 1
+                                    print("Right reps:", right_counter)
+                                right_stage = "isometric top"
+                            elif 60 <= right_angle <= 160:
+                                if right_stage == "isometric bottom":
+                                    right_stage = "concentric"
+                                elif right_stage == "isometric top":
+                                    right_stage = "eccentric"
+
+                    # Draw angle on the image
+                    if exercise_mode in ['biceps', 'triceps']:
+                        left_position = left_elbow
+                        right_position = right_elbow
+                    else:
+                        left_position = left_knee
+                        right_position = right_knee
+
+                    cv2.putText(image, str(int(left_angle)),
+                                tuple(np.multiply(left_position, [640, 480]).astype(int)),
+                                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA
+                                )
+
+                    cv2.putText(image, str(int(right_angle)),
+                                tuple(np.multiply(right_position, [640, 480]).astype(int)),
+                                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA
+                                )
+
+                    # Draw landmarks and connections on the image
+                    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+                except Exception as e:
+                    print(e)
+                    continue
 
             # Display the image
             cv2.imshow('MediaPipe Pose', image)
@@ -131,13 +246,14 @@ def pose_detection():
 
 @app.route('/start', methods=['POST'])
 def start_pose_detection():
-    global left_counter, right_counter, left_stage, right_stage
+    global left_counter, right_counter, left_stage, right_stage, exercise_mode
     left_counter = 0
     right_counter = 0
     left_stage = None
     right_stage = None
+    exercise_mode = request.json.get('mode', 'biceps')
     threading.Thread(target=pose_detection).start()
-    return jsonify({'message': 'Pose detection started'})
+    return jsonify({'message': 'Pose detection started', 'mode': exercise_mode})
 
 @app.route('/stop', methods=['POST'])
 def stop_pose_detection():
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,13 +6,14 @@ function App() {
   const [leftReps, setLeftReps] = useState(0);
   const [rightReps, setRightReps] = useState(0);
 
-  const startScript = async () => {
+  const startScript = async (mode: string) => {
     try {
       const response = await fetch("http://localhost:5000/start", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
+        body: JSON.stringify({ mode }),
       });
       if (!response.ok) {
         throw new Error("Network response was not ok");
@@ -60,7 +61,10 @@ function App() {
   return (
     <div>
       <h1>Pose Detection</h1>
-      <button onClick={startScript}>Start</button>
+      <button onClick={() => startScript("biceps")}>Start Biceps</button>
+      <button onClick={() => startScript("triceps")}>Start Triceps</button>
+      <button onClick={() => startScript("quads")}>Start Quads</button>
+      <button onClick={() => startScript("hamstrings")}>Start Hamstrings</button>
       <button onClick={stopScript}>Stop</button>
       <div>
         <h2>Left Arm</h2>
